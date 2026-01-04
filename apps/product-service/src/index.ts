1a1204d18c143693b64272a7bf0881d5cd99ee4d
bimal009 <pandeybimal616@gmail.com>
2026-01-04 22:18:37 +0545

    fix(index): remove duplicate express.json() middleware usage

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -23,8 +23,9 @@ app.use(cors({
     origin:["http://localhost:3000","http://localhost:3001"],
     credentials:true
 }))
+app.use(express.json());
+
 app.use(limiter)
-app.use(express.json())
 
 app.get("/health",(req:Request,res:Response)=>{
     res.status(200).json({
